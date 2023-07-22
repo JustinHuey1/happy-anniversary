43765408394af964a9286ea401c0d5c3d92cfd58
Justin Huey <justinhuey01@berkeley.edu>
2023-07-22 02:41:20 -0700

    debug

--- Script.js
+++ Script.js
@@ -1,10 +1,10 @@
 var anniversaryDate = new Date("2022-07-22");
-var anniversaryMonth = anniversaryDate.getMonth();
-var anniversaryDay = anniversaryDate.getDate();
+var anniversaryMonth = anniversaryDate.getUTCMonth();
+var anniversaryDay = anniversaryDate.getUTCDate();
 
 var todaysDate = new Date();
-var todaysMonth = todaysDate.getMonth();
-var todaysDay = todaysDate.getDate();
+var todaysMonth = todaysDate.getUTCMonth();
+var todaysDay = todaysDate.getUTCDate();
 
 if (anniversaryMonth === todaysMonth && anniversaryDay === todaysDay){
     alert("HAPPY ANNIVERSARY BEBE!!!");
@@ -15,6 +15,11 @@ console.log(anniversaryDay);
 console.log(todaysMonth);
 console.log(todaysDay);
 
-$("#confirm").click(function(){
-
-})
+function confirmClick() {
+    let text = "I love you! Do you love me?\nOk for yes and cancel for no";
+    if (confirm(text) == true) {
+        alert("I love you moreeeeeeeeeeeeeeeeeeeeeee");
+      } else {
+        confirmClick();
+      }
+};
