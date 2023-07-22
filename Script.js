e07a39987c63da2fe11c25a484b15287308b81b5
Justin Huey <justinhuey01@berkeley.edu>
2023-07-22 02:51:15 -0700

    added timer and confirm button

--- Script.js
+++ Script.js
@@ -15,11 +15,30 @@ console.log(anniversaryDay);
 console.log(todaysMonth);
 console.log(todaysDay);
 
-function confirmClick() {
-    let text = "I love you! Do you love me?\nOk for yes and cancel for no";
-    if (confirm(text) == true) {
+function updateCountdown() {
+    var countDownDate = anniversaryDate.getTime();
+    var x = setInterval(function() {
+        var now = new Date().getTime();
+            
+        var distance = countDownDate - now;
+            
+        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+            
+        $("#timer").text(days + "d " + hours + "h " + minutes + "m " + seconds + "s " + "until our anniversary");
+            
+        if (distance < 0) {
+            $("#timer").text("OUR ANNIVERSARY IS TODAY!");
+        }
+}, 1000);
+}
+
+function confirmClick(prompt) {;
+    if (confirm(prompt) == true) {
         alert("I love you moreeeeeeeeeeeeeeeeeeeeeee");
       } else {
-        confirmClick();
+        confirmClick('I SAID I LOVE YOU!!!!! DO YOU LOVE ME HUH HUH?\nOk for yes and cancel for no');
       }
 };
