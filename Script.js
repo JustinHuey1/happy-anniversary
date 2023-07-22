e91b510bbe4ba558b5cb215307a406b46334e5d5
Justin Huey <justinhuey01@berkeley.edu>
2023-07-22 01:59:58 -0700

    test alert

--- Script.js
+++ Script.js
@@ -1,1 +1,11 @@
+var anniversaryDate = new Date("2022-07-22");
+var anniversaryMonth = anniversaryDate.getMonth();
+var anniversaryDay = anniversaryDate.getDate();
 
+var todaysDate = new Date();
+var todaysMonth = todaysDate.getMonth();
+var todaysDay = todaysDate.getDate();
+
+if (anniversaryMonth === todaysMonth && anniversaryDay === todaysDay){
+    alert("HAPPY ANNIVERSARY BEBE!!!")
+}
