c9257c9427c8a3ff7c105adc735ca0e28521f401
Justin Huey <justinhuey01@berkeley.edu>
2023-07-28 23:43:48 -0700

    Fixed a bug with the alert and countdown

--- Script.js
+++ Script.js
@@ -6,6 +6,12 @@ var todaysDate = new Date();
 var todaysMonth = todaysDate.getMonth();
 var todaysDay = todaysDate.getDate();
 
+var y = setInterval(function() {
+  todaysDate = new Date();
+  todaysMonth = todaysDate.getMonth();
+  todaysDay = todaysDate.getDate();
+}, 1000)
+
 var countDownDate = anniversaryDate.getTime();
 var now = new Date().getTime();
 
@@ -15,33 +21,17 @@ while(countDownDate - now < 0) {
   countDownDate = anniversaryDate.getTime();
 }
 
-if (anniversaryMonth === todaysMonth && anniversaryDay === todaysDay){
-    alert("HAPPY ANNIVERSARY BEBE!!!");
+var trigger = false;
 
-    function newHeart () {
-        var hearts = $('.heart');
-        if (hearts.length >= 24) return setTimeout(newHeart, 1000);
-        var c = $('.heart-container:first').clone();
-        var anims = ['swing', 'spin'];
-        var a = anims[Math.round(Math.random())];
-        c.find('.heart')
-                    .css('opacity', 0.2 + Math.random() * 0.8)
-                    .css('-webkit-animation-name', a)
-                    .css('-moz-animation-name', a);
-        c.css('left', (10 + Math.random() * ($('body').innerWidth() - 10)) + 'px');
-        c.css('-webkit-animation-name', 'fall');
-        c.css('-webkit-animation-duration', (4 + Math.random() * 14) + 's');
-        c.css('-webkit-transform', 'scale(' + (0.4 + Math.random() * 2) + ')');
-        c.css('-moz-animation-duration', (4 + Math.random() * 14) + 's');
-        c.css('-moz-transform', 'scale(' + (0.4 + Math.random() * 2) + ')');
-        $('#hearts').append(c);
-        c.bind('animationend', function () { c.remove(); });
-        c.bind('webkitAnimationEnd', function () { c.remove(); });
-        setTimeout(newHeart, 200);
+var y = setInterval(function() {
+  if (!trigger) {
+    if (anniversaryMonth === todaysMonth && anniversaryDay === todaysDay){
+      alert("HAPPY ANNIVERSARY BEBE!!!");
+
+      trigger = true;
     }
-
-    setTimeout(newHeart, 200);
-}
+  }
+}, 1000)
 
 
 var x = setInterval(function() {
@@ -58,8 +48,32 @@ var x = setInterval(function() {
         
     if (anniversaryMonth === todaysMonth && anniversaryDay === todaysDay) {
         $("#countdown").html("OUR ANNIVERSARY IS TODAY!");
+
+        function newHeart () {
+          var hearts = $('.heart');
+          if (hearts.length >= 24) return setTimeout(newHeart, 1000);
+          var c = $('.heart-container:first').clone();
+          var anims = ['swing', 'spin'];
+          var a = anims[Math.round(Math.random())];
+          c.find('.heart')
+                      .css('opacity', 0.2 + Math.random() * 0.8)
+                      .css('-webkit-animation-name', a)
+                      .css('-moz-animation-name', a);
+          c.css('left', (10 + Math.random() * ($('body').innerWidth() - 10)) + 'px');
+          c.css('-webkit-animation-name', 'fall');
+          c.css('-webkit-animation-duration', (4 + Math.random() * 14) + 's');
+          c.css('-webkit-transform', 'scale(' + (0.4 + Math.random() * 2) + ')');
+          c.css('-moz-animation-duration', (4 + Math.random() * 14) + 's');
+          c.css('-moz-transform', 'scale(' + (0.4 + Math.random() * 2) + ')');
+          $('#hearts').append(c);
+          c.bind('animationend', function () { c.remove(); });
+          c.bind('webkitAnimationEnd', function () { c.remove(); });
+          setTimeout(newHeart, 200);
+      }
+  
+      setTimeout(newHeart, 200);
     }
-}, 1000);
+  }, 1000);
 
 var huhCounter = 0;
 
